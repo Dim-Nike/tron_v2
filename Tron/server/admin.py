f2ba1f2889dec847844c37902567bb8aaa07cc16
artyom <artyom27000@mail.ru>
2024-03-14 14:54:39 +0300

    [ADD] -> add abstract class User

--- Tron/server/admin.py
+++ Tron/server/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('login', 'IDF', 'tariff')  # Поля, которые будут отображаться в списке пользователей
+    search_fields = ('login', 'IDF', 'tariff')  # Поля, по которым можно будет осуществлять поиск
+
+admin.site.register(User, UserAdmin)
--- Tron/server/models.py
+++ Tron/server/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, login, password=None, **extra_fields):
+        if not login:
+            raise ValueError('The Login field must be set')
+
+        user = self.model(login=login, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, login, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(login, password, **extra_fields)
+
+
+
+class User(AbstractBaseUser):
+    login = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)
+    IDF = models.CharField(max_length=500)
+    tariff = models.CharField(max_length=50)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'login'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.login
